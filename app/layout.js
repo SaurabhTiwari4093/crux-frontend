b7427cef88bb597b94a3c245fb293e8708bb0656
Saurabh Tiwari <saurabhtiwari4093@gmail.com>
2024-02-18 14:49:09 +0530

    Basic frontend

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,15 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Crux",
   description: "This app lets the employer find the most suitable candidates amongst a list of resumes.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en" className="text-gray-800">
       <body className={inter.className}>
@@ -24,3 +19,4 @@ export default function RootLayout({
     </html>
   );
 }
+
